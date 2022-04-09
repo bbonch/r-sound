4d05216233a03d42d623fc83c9258e3bdef5618f
Dmitry Sergienko <dmitry.sergienko@codefirst.net>
2022-04-09 20:51:29 +0400

    Added logic

--- RSound/RSound/Program.cs
+++ RSound/RSound/Program.cs
@@ -1,1 +1,36 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Media;
+
+var soundFile = "default.wav";
+var time = 5;
+
+if (args.Length > 0)
+{
+    if (int.TryParse(args[0], out int timeArgs))
+    {
+        time = timeArgs;
+    }
+}
+if (args.Length > 1)
+{
+    soundFile = args[1];
+}
+
+var start = DateTime.Now;
+var end = start.AddMinutes(time);
+var random = new Random();
+
+while (DateTime.Now < end)
+{
+    PlaySound(soundFile);
+
+    var sleep = random.Next(10, 30);
+    Thread.Sleep(1000 * sleep);
+}
+
+PlaySound(soundFile);
+
+static void PlaySound(string soundFile)
+{
+    var player = new SoundPlayer(soundFile);
+    player.PlaySync();
+}
